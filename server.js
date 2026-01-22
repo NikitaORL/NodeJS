8f25832c3a211c7d45a99ac4ca2f8b212058b14c
Opilane TTHK <opilane@tthk.ee>
2026-01-22 15:03:43 +0200

    Oli tehtud 404 viga

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
-  res.write('<h1>Tervitused serverist</h1>');
-  res.write('<p>See tekst tuli Node.js serverist</p>');
-  res.end();
+    res.setHeader('Content-Type', 'text/html');
+
+
+    if (req.url === '/') {
+        filePath = './views/index.html';
+    } else if (req.url === '/services') {
+        filePath = './views/services.html';
+    } else if (req.url === '/teenused') {
+        filePath = './views/teenused.html';
+    } else {
+        filePath = './views/404.html';
+        res.statusCode = 404; // страница не найдена
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 500; // ошибка сервера
+            res.end('Ошибка сервера');
+        } else {
+            res.end(data); // отправляем содержимое HTML
+        }
+    });
 });
 
 server.listen(3000, () => {
